6006546093e24c5124f5845859f0f00cc5538ceb
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2025-08-01 08:34:12 +0200

    feat: add graphql gateway content configuration service provider and update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,29 +1,32 @@
-import { AccountEntityContextProvider } from './entity-context-provider/account-entity-context-provider.service.js';
-import { KubernetesServiceProvidersService } from './service-providers/kubernetes-service-providers.service.js';
-import { Module } from '@nestjs/common';
-import { PortalModule, PortalModuleOptions } from '@openmfp/portal-server-lib';
-import { config } from 'dotenv';
+import {AccountEntityContextProvider} from './entity-context-provider/account-entity-context-provider.service.js';
+import {Module} from '@nestjs/common';
+import {PortalModule, PortalModuleOptions} from '@openmfp/portal-server-lib';
+import {config} from 'dotenv';
 import * as path from 'node:path';
+import {
+    ContentConfigurationServiceProvidersService
+} from "./service-providers/content-configuration-service-providers.service.js";
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
 
-config({ path: './.env' });
+config({path: './.env'});
 
 const portalOptions: PortalModuleOptions = {
-  frontendDistSources: path.join(
-    __dirname,
-    '../..',
-    'frontend/dist/frontend/browser',
-  ),
-  entityContextProviders: {
-    account: AccountEntityContextProvider,
-  },
-  additionalProviders: [AccountEntityContextProvider],
-  serviceProviderService: KubernetesServiceProvidersService,
+    frontendDistSources: path.join(
+        __dirname,
+        '../..',
+        'frontend/dist/frontend/browser',
+    ),
+    entityContextProviders: {
+        account: AccountEntityContextProvider,
+    },
+    additionalProviders: [AccountEntityContextProvider],
+    serviceProviderService: ContentConfigurationServiceProvidersService,
 };
 
 @Module({
-  imports: [PortalModule.create(portalOptions)],
+    imports: [PortalModule.create(portalOptions)],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ backend/src/service-providers/content-configuration-service-providers.service.ts
@@ -0,0 +1,71 @@
+import {
+    ContentConfiguration,
+    ServiceProviderResponse,
+    ServiceProviderService,
+} from '@openmfp/portal-server-lib';
+import {GraphQLClient, gql} from 'graphql-request';
+import {ContentConfigurationQueryResponse} from "./models/contentconfigurations.js";
+
+export const contentConfigurationsQuery = gql`
+    query {
+        core_openmfp_io {
+            ContentConfigurations { metadata { name labels } spec { remoteConfiguration { url } } status { configurationResult }}
+        }
+    }
+`;
+
+export class ContentConfigurationServiceProvidersService
+    implements ServiceProviderService {
+    private vsGatewayBaseUrl: string;
+
+    constructor() {
+        const gatewayUrl = process.env.OPENMFP_PORTAL_CONTEXT_CRD_GATEWAY_API_URL;
+        this.vsGatewayBaseUrl = gatewayUrl.split('/').slice(0, -2).join('/') + `/virtual-workspace/contentconfigurations`
+    }
+
+    async getServiceProviders(
+        token: string,
+        entities: string[],
+        context: Record<string, any>
+    ): Promise<ServiceProviderResponse> {
+        let path = `/root:orgs:${context.organization}`;
+        if (context?.account) {
+            path += `:${context.account}`;
+        }
+        const gatewayPath = `${this.vsGatewayBaseUrl}${path}/graphql`
+        const client = new GraphQLClient(gatewayPath, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        const response = await client.request<ContentConfigurationQueryResponse>(
+            contentConfigurationsQuery, {}
+        );
+
+        const entity = !entities || !entities.length ? 'main' : entities[0];
+        let contentConfigurations = response.core_openmfp_io.ContentConfigurations
+            .filter((item) => item.metadata.labels?.["portal.openmfp.org/entity"] === entity
+            )
+            .map((item) => {
+                const contentConfiguration = JSON.parse(
+                    item.status.configurationResult
+                ) as ContentConfiguration;
+                if (!contentConfiguration.url) {
+                    contentConfiguration.url = item.spec.remoteConfiguration?.url;
+                }
+                return contentConfiguration;
+            });
+
+        return {
+            rawServiceProviders: [
+                {
+                    name: 'openmfp-system',
+                    displayName: '',
+                    creationTimestamp: '',
+                    contentConfiguration: contentConfigurations,
+                },
+            ],
+        };
+    }
+}
