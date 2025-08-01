802b1946f594a01151a2c41ac8c9fa9787cb2a04
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-08-01 16:25:12 +0200

    fix tests

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -6,17 +6,8 @@ module.exports = {
   },
   rootDir: 'src',
   testRegex: '.spec.ts$',
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputName: 'TEST-unit-tests-results.xml',
-        outputDirectory: 'test-run-reports',
-      },
-    ],
-  ],
-  coverageDirectory: '../test-run-reports/coverage/unit',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
   resolver: '<rootDir>/../jest-custom-resolver.cjs',
   moduleNameMapper: {
     '^@openmfp/portal-server-lib$':
